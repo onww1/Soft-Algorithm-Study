51ae4bc998fa708c0bbc601a03b497479ae36621
DaleBae <jermy0131@gmail.com>
2019-01-28 15:43:49 +0900

    Create Algwang_2018.cpp

---
+++ week04/boj2018/Algwang_2018.cpp
@@ -0,0 +1,39 @@
+/*
+ * 시간복잡도 : O(N)
+ * 공간복잡도 : O(1)
+ * 
+ * 연속된 숫자의 합이라는 성질을 이용했습니다.
+ * 특정 수 n의 절반+1까지 탐색을 하며 지금까지의 합에서 n을 뺀 값이
+ * 등차수열을 만족하는 경우를 찾아서 count를 늘려줬습니다.
+ * 마지막에 +1하는 부분은 자기 자신의 경우입니다.
+ * 5 + 6 + 7 + 8은 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8에서 1 + 2 + 3 + 4를 뺀 값이라는
+ * 성질을 이용했습니다.
+*/
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(void){
+    ll n;
+    cin>>n;
+
+    int count=0;
+    
+    for(int i=1;i<=n/2+1;i++){
+        ll temp=((ll)i*(ll)(i+1))/2;
+        if(temp-n>=0){
+            ll myT=(ll)sqrt((temp-n)*2);
+            if(1ll*myT*(myT+1)==1ll*(temp-n)*2){
+                count++;
+            }
+        }
+    }
+    if(n==1||n==2){
+        cout<<1<<endl;
+        return 0;
+    }
+    cout<<count+1<<endl;
+}
