07ad272edab4a4b3a12e1f95c0317aa4ed05f10e
oridya <38060133+orihehe@users.noreply.github.com>
2019-02-22 16:40:07 +0900

    Add orihehe week7 codes

---
+++ week07/boj1280/orihehe_1280.cpp
@@ -0,0 +1,78 @@
+﻿/*
+BOJ 1280 - 나무 심기
+
+시간복잡도 : O(Nlog200000)
+공간복잡도 : O(2^log200000)
+
+세그트리의 각 노드에 구간에 심어진 나무 개수, s부터 구간에 심어진 나무 거리,
+r부터 구간에 심어진 나무 거리 정보를 저장합니다.
+나무를 심을 때마다 현재 나무의 왼쪽 오른쪽을 나누어 hap에서 거리를 구해줍니다.
+그후 나무를 update에서 심어줍니다.
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+#define mod 1000000007
+#define MAX 200002
+using namespace std;
+
+/* 🐣🐥 */
+struct info {
+	ll num, lv, rv;
+	info() {
+		num = lv = rv = 0;
+	}
+};
+info *tree;
+int arr[100002];
+void update(int idx, int s, int e, int k) { // 현재 나무 k위치에 심는다.
+	if (k < s || k > e) return;
+	tree[idx].num++; // 현재 구간의 심어진 나무 수
+	tree[idx].lv += k - s; // lv에 s부터 k까지의 거리 더함
+	tree[idx].rv += e - k; // rv에 r부터 k까지의 거리 더함
+	if (s == e) return;
+	update(idx * 2, s, (e + s) / 2, k);
+	update(idx * 2 + 1, (e + s) / 2 + 1, e, k);
+}
+ll hap(int idx, int s, int e, int l, int r, int k) {
+	if (l > e || r < s) return 0;
+	ll tmp;
+	if (l <= s && e <= r) {
+		// 현재 구간 위치의 오른쪽인지 왼쪽인지, 가까운쪽
+		if (abs(s - k) < abs(e - k)) { // 왼쪽에 심어졌다면
+			tmp = abs(s - k);
+			// k~s거리 * 심어진 나무 수 + lv
+			return ((tree[idx].num * tmp)%mod + tree[idx].lv)%mod;
+		}
+		else {
+			tmp = abs(e - k);
+			// e~k거리 * 심어진 나무 수 + rv
+			return ((tree[idx].num * tmp)%mod + tree[idx].rv)%mod;
+		}
+	}
+	return hap(idx * 2, s, (e + s) / 2, l, r, k)
+		+ hap(idx * 2 + 1, (e + s) / 2 + 1, e, l, r, k);
+}
+int main() {
+	int n, h, x;
+	ll ans = 1, tmp;
+	scanf("%d", &n);
+	h = ceil(log2(MAX)) + 1;
+	tree = new info[1 << h];
+	fill(tree, tree + (1 << h), info());
+	scanf("%d", &x);
+	update(1, 1, MAX, x + 1);
+	for (int i = 1; i < n; i++) {
+		scanf("%d", &x);
+		// x좌표가 0~200000이기 때문에 구간을 1부터 표시하려 +1한 값을 넣었습니다.
+		// 현재 나무 위치의 왼쪽, 오른쪽 거리 합
+		tmp = hap(1, 1, MAX, 1, x + 1, x + 1)
+			+ hap(1, 1, MAX, x + 1, MAX, x + 1);
+		ans = (ans*tmp) % mod;
+		update(1, 1, MAX, x + 1);
+	}
+	printf("%lld", ans);
+
+	return 0;
+}
---
+++ week07/boj3653/orihehe_3653.cpp
@@ -0,0 +1,60 @@
+﻿/*
+BOJ 3653 - 영화 수집
+
+시간복잡도 : O(Mlog(N+M)
+공간복잡도 : O(2^log(200000))
+
+트리를 N제한으로만 짜려고만 생각하다 오래 걸렸습니다ㅠㅠ
+세그트리의 리프노드를 생각했을 때 1~n의 맨 오른쪽을 맨 위라고 생각합니다.
+arr배열에 각 DVD의 인덱스를 저장해두고, n=5일때 5,4,3,2,1로 DVD를 쌓아둡니다. 
+그리고나서, 선택한 DVD의 인덱스~최고 높이 까지의 합을 출력해주고 
+그 DVD를 삭제 후 최고높이 +1에 쌓아줍니다.
+*/
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#define MAX 200001
+using namespace std;
+
+/* 🐣🐥 */
+int *tree;
+int arr[100001];
+void update(int idx, int s, int e, int k, int val) {
+	if (k < s || k > e) return;
+	tree[idx]+=val;
+	if (s == e) return;
+	update(idx * 2, s, (e + s) / 2, k, val);
+	update(idx * 2 + 1, (e + s) / 2 + 1, e, k,val);
+	tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+}
+int hap(int idx, int s, int e, int l, int r) {
+	if (l > e || r < s) return 0;
+	if (l <= s && e <= r) return tree[idx];
+	return hap(idx * 2, s, (e + s) / 2, l, r)
+		+ hap(idx * 2 + 1, (e + s) / 2 + 1, e, l, r);
+}
+int main() {
+	int t, n, m, h, end, x;
+	scanf("%d", &t);
+	h = ceil(log2(MAX)) + 1;
+	tree = new int[1 << h];
+	while (t--) {
+		scanf("%d %d", &n, &m);
+		fill(tree, tree + (1 << h), 0);
+		end = n; // 최고 높이
+		for (int i = 1; i <= n; i++) {
+			update(1, 1, n + m, i, 1); // 각 DVD 쌓아줌
+			arr[i] = n - i + 1; 
+		}
+		for (int i = 0; i < m; i++) {
+			scanf("%d", &x);
+			printf("%d ", hap(1, 1, n + m, arr[x], end)-1);
+			update(1, 1, n + m, arr[x], -1); // 현재 DVD삭제
+			arr[x] = ++end; // 높이 증가
+			update(1, 1, n + m, arr[x], 1); // 현재 DVD 맨 위에
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
